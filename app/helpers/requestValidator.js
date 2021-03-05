492bedb51e9fa90780d5062774112bb63001cc88
Kumaranath Fernando <wkk91193@yahoo.com>
2021-03-05 19:55:55 +0700

    Validating API calls

---
+++ app/helpers/requestValidator.js
@@ -0,0 +1,35 @@
+const { body, param } = require('express-validator/check');
+const requestMessage =require('../consts/responseMessages.json');
+
+exports.validate = (method) => {
+    switch (method) {
+      case 'createTask': {
+       return [ 
+          body('title', requestMessage['response.title.required']).exists()
+         ]
+        break;   
+      }    
+      case 'findTaskById':{
+        return[
+            param('taskId',requestMessage['response.taskId.required']).exists(),
+            param('taskId').isNumeric()
+          ]
+        break;
+      }
+      case 'updateTaskStatus':{
+        return[
+            param('taskId', requestMessage['response.taskId.required']).exists(),
+            body('title', requestMessage['response.title.required']).exists(),
+            body('taskStatus', requestMessage['response.taskStatus.required']).exists(),
+          ]
+        break;
+      }
+      case 'deleteTask':{
+        return[
+          param('taskId',requestMessage['response.taskId.required']).exists(),
+          param('taskId').isNumeric()
+        ]
+        break;
+      }
+    }
+  }
