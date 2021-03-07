cc2f30adf87cbfc15d31a89ad5de0fddbf58badb
Kumaranath Fernando <wkk91193@yahoo.com>
2021-03-07 14:14:32 +0700

    Modifying the dbConfig file to accept new parameters

--- app/models/index.js
+++ app/models/index.js
@@ -1,17 +1,22 @@
 const dbConfig = require("../config/db.config.js");
 
 const Sequelize = require("sequelize");
-const sequelize = new Sequelize(dbConfig.DB, dbConfig.USER, dbConfig.PASSWORD, {
-  host: dbConfig.HOST,
-  dialect: dbConfig.dialect,
-
-  pool: {
-    max: dbConfig.pool.max,
-    min: dbConfig.pool.min,
-    acquire: dbConfig.pool.acquire,
-    idle: dbConfig.pool.idle
-  }
-});
+const sequelize = new Sequelize(
+  dbConfig.DB,
+  dbConfig.USER,
+  dbConfig.PASSWORD,
+  {
+    host: dbConfig.HOST,
+    port: dbConfig.PORT,
+    maxConcurrentQueries: dbConfig.MAX_CONCURRENT_QUERIES,
+    ssl: dbConfig.SSL,
+    pool: {
+      maxConnections: dbConfig.pool.max,
+      maxIdleTime: dbConfig.pool.idle
+    },
+    language: dbConfig.language,
+    dialect: dbConfig.dialect,
+  });
 
 const db = {};
 
@@ -20,5 +25,9 @@ db.sequelize = sequelize;
 
 db.task = require("./task.model.js")(sequelize, Sequelize);
 
+// force: true will drop the table if it already exists
+// db.sequelize.sync({force: false}).then(() => {
+//   console.log('Drop and Resync with { force: true }');
+// });
 
 module.exports = db;
